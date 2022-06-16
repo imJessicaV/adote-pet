7aeddc398db5d7366b6b8178a9781867b4ac7845
imJessicaV <jessica.svitoriano@gmail.com>
2022-06-16 12:32:48 -0300

    Adicionando elementos de estilo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//Pagina principal de toda a aplicação. O que for configurado aqui, aparecerá em todas as paginas do app
+
+import "../ui/styles/global.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import tema from "../ui/themes/tema";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={tema}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/ui/components/Header/Header.style.tsx
@@ -0,0 +1,5 @@
+import { styled } from "@mui/material";
+
+styled('header')`
+    color: red;
+`;
